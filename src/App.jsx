1e3a861d96f0be931fcaeb7c400d1509ff8bf32d
ConnaaaR69 <connormattless@gmail.com>
2023-11-22 22:38:29 +0000

    init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+	return (
+		<>
+			<button className="btn btn-outline btn-info">Button</button>
+		</>
+	);
+}
+
+export default App;
---
+++ tailwind.config.cjs
@@ -0,0 +1,43 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [require("daisyui")],
+    daisyui: {
+        themes: [
+          {
+            cupcake: {
+          ...require("daisyui/src/theming/themes")["cupcake"],
+          "primary": "#A0C4FF",
+          "secondary": "#BDB2FF",
+          "accent": "#FDFFB6",
+          "neutral": "#1a1a9f",
+          "base-100": "#FFFFFC",
+          "info": "#1a1a9f",
+          "success": "#adf7b6",
+          "warning": "#FFD6A5",
+          "error": "#FFADAD",
+    
+          ".menu-active": {
+            "background-color": "#1a1a9f",
+            "color": "#FFFFFC",
+          }
+                    }}, 
+            {synthwave:{
+          ...require("daisyui/src/theming/themes")["synthwave"],
+          "primary": "#A0C4FF",
+          "secondary": "#BDB2FF",
+          "accent": "#FDFFB6",
+          "neutral": "#1a1a9f",
+          "base-100": "#1a103d",
+          "info": "#1a1a9f",
+          "success": "#adf7b6",
+          "warning": "#FFD6A5",
+          "error": "#FFADAD",
+                    }
+          }]
+        
+      },
+  }
