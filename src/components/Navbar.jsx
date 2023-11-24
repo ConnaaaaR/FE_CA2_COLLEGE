cdd76597657cd20e5730e33b0d5b41e3846ad9a9
ConnaaaR69 <connormattless@gmail.com>
2023-11-24 22:08:05 +0000

    mass deletion functionalty

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -27,30 +27,14 @@ const Navbar = () => {
 	return (
 		<>
 			<div className="navbar bg-base-100">
-				<div className="flex-none">
-					<button className="btn btn-square btn-ghost">
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							fill="none"
-							viewBox="0 0 24 24"
-							className="inline-block w-5 h-5 stroke-current"
-						>
-							<path
-								strokeLinecap="round"
-								strokeLinejoin="round"
-								strokeWidth="2"
-								d="M4 6h16M4 12h16M4 18h16"
-							></path>
-						</svg>
-					</button>
-				</div>
+				<div className="flex-none"></div>
 				<div className="flex-1">
 					<Link to="/" className="btn btn-ghost text-xl">
 						class<span className="text-success-content">Coord</span>
 					</Link>
-					<Link to="/courses" className="btn btn-ghost text-xl">
+					{/* <Link to="/courses" className="btn btn-ghost text-xl">
 						Courses
-					</Link>
+					</Link> */}
 				</div>
 				<div className="flex-none gap-8">
 					<div className="flex-none gap-8">
--- src/pages/courses/Index.jsx
+++ src/pages/courses/Index.jsx
@@ -6,7 +6,13 @@ import { Link } from "react-router-dom";
 const Index = () => {
 	const { authenticated } = useAuth();
 	const [courses, setCourses] = useState([]);
+	const [selectedCourses, setSelectedCourses] = useState([]);
 	const [loading, setLoading] = useState(false);
+	const [isModalOpen, setIsModalOpen] = useState(false);
+	const [courseHasEnrollments, setCourseHasEnrollments] = useState(false);
+
+	const openModal = () => setIsModalOpen(true);
+	const closeModal = () => setIsModalOpen(false);
 	let token = localStorage.getItem("token");
 
 	useEffect(() => {
@@ -26,6 +32,49 @@ const Index = () => {
 				setLoading(false);
 			});
 	}, []);
+
+	// debug useEffect for seeing deletion IDs
+	useEffect(() => {
+		console.log(selectedCourses);
+	}, [selectedCourses]);
+
+	const toggleCourseSelection = (courseId) => {
+		setSelectedCourses((prevSelectedCourses) =>
+			prevSelectedCourses.includes(courseId)
+				? prevSelectedCourses.filter((id) => id !== courseId)
+				: [...prevSelectedCourses, courseId]
+		);
+	};
+
+	const deleteEnrollmentsAndCourse = async () => {
+		let token = localStorage.getItem("token");
+		// deletes enrollments, waits for it to complete and then deletes the course
+		try {
+			for (const courseId of selectedCourses) {
+				const course = courses.find((c) => c.id === courseId);
+				for (const enrollment of course.enrolments) {
+					await axios.delete(`/enrolments/${enrollment.id}`, {
+						headers: { Authorization: `Bearer ${token}` },
+					});
+				}
+				await axios.delete(`/courses/${courseId}`, {
+					headers: { Authorization: `Bearer ${token}` },
+				});
+			}
+			// Update the UI after successful deletion
+			setCourses(
+				courses.filter((course) => !selectedCourses.includes(course.id))
+			);
+			setSelectedCourses([]);
+		} catch (error) {
+			console.error("Deletion error:", error);
+		}
+	};
+
+	const handleDeleteConfirmation = () => {
+		closeModal();
+		deleteEnrollmentsAndCourse();
+	};
 
 	const SkeletonRow = () => {
 		return (
@@ -57,20 +106,25 @@ const Index = () => {
 	};
 
 	const courseSkeletons = [...Array(15)].map((_, index) => (
+		// value param is never used thus is named '_'
 		<SkeletonRow key={index} />
 	));
 
-	// if (loading) return <h3>Loading courses...</h3>;
 	if (!loading && courses.length === 0) return <h3>There are no courses!</h3>;
 	const coursesList = courses.map((course) => {
 		return (
 			<>
 				{authenticated ? (
 					<>
-						<tr key={course._id}>
+						<tr key={course.id}>
 							<th>
 								<label>
-									<input type="radio" className="radio radio-info" />
+									<input
+										type="checkbox"
+										className="checkbox checkbox-info"
+										checked={selectedCourses.includes(course.id)}
+										onChange={() => toggleCourseSelection(course.id)}
+									/>
 								</label>
 							</th>
 
@@ -119,10 +173,37 @@ const Index = () => {
 	return (
 		<>
 			<main className="container mx-auto max-w-7xl my-5">
+				<div className={`modal ${isModalOpen ? "modal-open" : ""}`}>
+					<div className="modal-box">
+						<h3 className="font-bold text-lg">
+							Are you sure you want to delete the selected courses?
+						</h3>
+
+						<p className="py-4">
+							Courses with enrollments will have their enrollments deleted as
+							well.
+						</p>
+
+						<div className="modal-action">
+							<button
+								onClick={handleDeleteConfirmation}
+								className="btn btn-error"
+							>
+								Yes, Delete
+							</button>
+							<button onClick={closeModal} className="btn btn-outline">
+								Cancel
+							</button>
+						</div>
+					</div>
+				</div>
 				<section className=" bg-base-300 rounded-2xl p-5 ">
 					<h2 className="text-3xl">All Courses</h2>
 
 					<div className="overflow-x-auto ">
+						<button onClick={openModal} className="btn btn-error flex-end">
+							Delete Selected
+						</button>
 						<table className="table ">
 							<thead>
 								<tr>
