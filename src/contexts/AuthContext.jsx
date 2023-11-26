f60f8c507f82f6a5afeecd4491b76148ebc94bda
ConnaaaR69 <connormattless@gmail.com>
2023-11-26 21:08:22 +0000

    auth changed to use useEffect for better login state handling

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,33 +1,35 @@
-import { useContext, createContext, useState } from "react";
+import { useContext, createContext, useState, useEffect } from "react";
 
-const AuthContext = createContext(null);
+const AuthContext = createContext({
+	authenticated: false,
+	onAuthenticated: () => {},
+});
 
 export function useAuth() {
-	const val = useContext(AuthContext);
-	return val;
+	return useContext(AuthContext);
 }
 
-export function AuthProvider(props) {
-	const [authenticated, setAuthenticated] = useState(
-		Boolean(localStorage.getItem("token"))
-	);
+export function AuthProvider({ children }) {
+	const [authenticated, setAuthenticated] = useState(false);
+
+	useEffect(() => {
+		const token = localStorage.getItem("token");
+		token ? setAuthenticated(true) : null;
+	}, []);
+
+	const onAuthenticated = (auth, token) => {
+		if (auth && token) {
+			localStorage.setItem("token", token);
+			setAuthenticated(true);
+		} else if (!auth) {
+			localStorage.removeItem("token");
+			setAuthenticated(false);
+		}
+	};
 
 	return (
-		<AuthContext.Provider
-			value={{
-				authenticated,
-				onAuthenticated: (auth, token) => {
-					setAuthenticated(true);
-
-					if (auth && token) {
-						localStorage.setItem("token", token);
-					} else if (!auth) {
-						localStorage.removeItem("token");
-					}
-				},
-			}}
-		>
-			{props.children}
+		<AuthContext.Provider value={{ authenticated, onAuthenticated }}>
+			{children}
 		</AuthContext.Provider>
 	);
 }
